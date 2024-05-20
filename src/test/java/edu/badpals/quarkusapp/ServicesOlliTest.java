8923f13debf16fe72ff68e84d091b378e9c0d26e
yelkov <yelkovq@gmail.com>
2024-05-20 14:00:33 +0200

    feat(ServiceOlli.java): añadir condicion destreza>quality

--- src/test/java/edu/badpals/quarkusapp/ServicesOlliTest.java
+++ src/test/java/edu/badpals/quarkusapp/ServicesOlliTest.java
@@ -226,7 +226,7 @@ public class ServicesOlliTest {
      * cuando la destreza de la usuaria sea menor
      * que la calidad del Item.
      */
-    /*@Test
+    @Test
     public void test_comanda_item_sin_pro() {
         Assertions.assertThat(servicio).isNotNull();
         Orden orden = servicio.comanda("Doobey", "+5 Dexterity Vest");
@@ -234,7 +234,7 @@ public class ServicesOlliTest {
 
         Orden pedido = em.find(Orden.class, 3L);
         Assertions.assertThat(pedido).isNull();
-    }*/
+    }
 
     /**
      * Implementa el metodo comandaMultiple para que una usuaria
